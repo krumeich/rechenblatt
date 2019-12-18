7b31a59bd850d649ce6c48e0ea3d2ba65e6f9c90
Alexander Krumeich <alexander@krumeich.de>
2019-12-18 19:22:29 +0100

    Aufbau der Seite komplett in Lua Code ausgelagert

---
+++ template.lua
@@ -0,0 +1,28 @@
+template={}
+
+function template.print_header(tex)
+   tex.print([[\begin{center}\textsf{{\Large Rechenblatt}\\Zahlenraum bis 100\\Kein Zehnerübergang}\end{center}\vspace{1cm}]])
+end
+
+function template.print_table_env_begin(tex)
+   tex.print([[\begin{tabular}{R@{\hspace{3cm}}R@{\hspace{3cm}}R}]])
+end   
+
+function template.print_table_env_end(tex)
+  tex.print([[\end{tabular}\pagebreak]])
+end   
+
+function template.print_aufgaben(tex)
+   for i=1, 15 do
+      tex.print("$")
+      tex.print(rechnen.create(100,10))
+      tex.print("$ & $")
+      tex.print(rechnen.create(100,10))
+      tex.print("$ & $")
+      tex.print(rechnen.create(100,10))
+      tex.print("$")
+      tex.print("\\\\[2ex]")
+   end
+end
+
+return template
