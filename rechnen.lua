d25891b6f1e31249c78e0e35427ba45e8dc2cfc5
Alexander Krumeich <alexander@krumeich.de>
2020-03-19 10:51:52 +0100

    Multiplikation bis 10^6

--- rechnen.lua
+++ rechnen.lua
@@ -7,6 +7,7 @@ rechnen={failed=0}
 local operations = {}
 operations["+"] = function(a,b) return a+b end;
 operations["-"] = function(a,b) return a-b end;
+operations["\\cdot"] = function(a,b) return a*b end;
 
 function rndop()
    math.random(); math.random(); math.random()
@@ -31,18 +32,18 @@ function kein_uebergang(z1, z2, operation, grenze)
 end
 
 function rechnen.create(j, k)
-   local z1 = math.random(j or 100)+100
-   local z2 = math.random(k or 100)+10
-   local operator = rndop()
-   local operation = operations[operator]
+   local z1 = math.random(j or 100)
+   local z2 = math.random(k or 100)
+   local operator = "\\cdot"
+   local operation = operations["\\cdot"]
    local aufgabe = {}
    table.insert(aufgabe, z1)
+   table.insert(aufgabe, "&")
    table.insert(aufgabe, operator)
+   table.insert(aufgabe, "&")
    table.insert(aufgabe, z2)
-   table.insert(aufgabe, " = ")
+   table.insert(aufgabe, "& = ")
    if ergebnis_positiv(operation(z1, z2))
-      and ergebnis_nicht_groesser_als(operation(z1, z2))
-      and kein_uebergang(z1, z2, operation, 100)
    then
       return table.concat(aufgabe)
    else
--- template.lua
+++ template.lua
@@ -1,11 +1,11 @@
 template={}
 
 function template.print_header(tex)
-   tex.print([[\begin{center}\textsf{{\Large Rechenblatt}\\Zahlenraum bis 1000\\Ohne Hunderterübergang}\end{center}\vspace{1cm}]])
+   tex.print([[\begin{center}\textsf{{\Large Rechenblatt}\\Zahlenraum bis 1.000.000}\end{center}\vspace{0.5cm}]])
 end
 
 function template.print_table_env_begin(tex)
-   tex.print([[\begin{tabular}{R@{\hspace{3cm}}R@{\hspace{3cm}}R}]])
+   tex.print([[\begin{tabular}{r@{\hspace{0.5em}}c@{\hspace{0.5em}}r@{\hspace{0.5em}}l@{\hspace{4cm}}r@{\hspace{0.5em}}c@{\hspace{0.5em}}r@{\hspace{0.5em}}l@{\hspace{4cm}}r@{\hspace{0.5em}}c@{\hspace{0.5em}}r@{\hspace{0.5em}}l}]])
 end   
 
 function template.print_table_env_end(tex)
@@ -14,15 +14,13 @@ end
 
 function template.print_aufgaben(tex)
    require "rechnen"
-   for i=1, 15 do
-      tex.print("$")
-      tex.print(rechnen.create(900,99))
-      tex.print("$ & $")
-      tex.print(rechnen.create(900,99))
-      tex.print("$ & $")
-      tex.print(rechnen.create(900,99))
-      tex.print("$")
-      tex.print("\\\\[2ex]")
+   for i=1, 8 do
+      tex.print(rechnen.create(999,999))
+      tex.print(" & ")
+      tex.print(rechnen.create(999,999))
+      tex.print(" & ")
+      tex.print(rechnen.create(999,999))
+      tex.print("\\\\[2.4cm]")
    end
 end
 
