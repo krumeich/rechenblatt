63b1fb19e9952a98f051c0ddc16873e4ebeeec06
Alexander Krumeich <alexander@krumeich.de>
2025-07-12 23:08:08 +0200

    refactor for more operations

--- rechnen.lua
+++ rechnen.lua
@@ -2,119 +2,38 @@
 
 math.randomseed( os.clock() * 1000000000 )
 
-rechnen={failed=0}
+rechnen={}
 
 local operations = {}
-operations["+"] = function(a,b) return a+b end;
-operations["-"] = function(a,b) return a-b end;
-operations["\\cdot"] = function(a,b) return a*b end;
+operations["add"] = function(a,b) return a+b end;
+operations["subt"] = function(a,b) return a-b end;
+operations["mult"] = function(a,b) return a*b end;
+operations["div"] = function(a,b) return a//b end;
 
-function rndop()
+function rechnen.negative_zahl()
    math.random(); math.random(); math.random()
    local o = math.random(2)
-   if o%2==1 then
-      return "+"
-   else
-      return "-"
-   end
+   return o%2==1
 end
 
-function ergebnis_positiv(z1)
-   return z1 > 0
-end
-
-function ergebnis_nicht_groesser_als(z1)
-   return z1 <= 1000
-end
-
-function kein_uebergang(z1, z2, operation, grenze)
-   return z1 // grenze == operation(z1,z2) // grenze
-end
-
-function rechnen.inbox(c)
-   local result = {}
-   table.insert(result, [[\fbox{\parbox[b][2ex][c]{1em}{\centering]])
-   table.insert(result, c)
-   table.insert(result, "}}")
-   return table.concat(result)
-end
-
-function rechnen.small(c)
-   local result = {}
-   table.insert(result, [[{\footnotesize \parbox[t][0ex][t]{1em}{\raggedleft]])
-   table.insert(result, c)
-   table.insert(result, "}}")
-   return table.concat(result)
-end
-
-function rechnen.printnumber(z, printer)
-   local result = {}
-   local p = printer or function(x) return x end
-   for c in string.gmatch(tostring(z), ".") do
-      table.insert(result, p(c))
-      table.insert(result, " & ")
-   end
-   return table.concat(result)
-end
-
-function rechnen.create(j, k)
-   local z1 = math.random(j or 100) +100
-   local z2 = math.random(k or 100) +100
-   local operator = "\\cdot"
-   local operation = operations["\\cdot"]
+function rechnen.create_aufgabe()
    local aufgabe = {}
-   table.insert(aufgabe, rechnen.printnumber(z1, rechnen.inbox))
-   table.insert(aufgabe, rechnen.inbox(operator))
-   table.insert(aufgabe, " & ")
-   table.insert(aufgabe, rechnen.printnumber(z2, rechnen.inbox))
-   if ergebnis_positiv(operation(z1, z2))
-   then
-      --      print (table.concat(aufgabe))
-      rechnen.kontrolle = operation(z1, z2)
-      return table.concat(aufgabe)
-   else
-      rechnen.failed = rechnen.failed+1
-      print(table.concat(aufgabe))
-      return rechnen.create()
-   end
-end
-
-function rechnen.hilfszeile(z)
-   local result = {}
-   pos = 4 - z
-   for i = 1, pos do
-      table.insert(result, " & ")
-   end
-   for i = 1, 4 do
-      table.insert(result, rechnen.inbox(" ~"))
-      table.insert(result, " & ")
-   end
-   return table.concat(result)
-end
-
-function rechnen.hilfsbereich()
-   local result= {}
-   for i = 1, 3 do
-      table.insert(result, rechnen.hilfszeile(i))
-      table.insert(result, [[\\]])
-   end
-   return table.concat(result)
-end
-      
-function rechnen.loesungszeile()
-   local result = {}
-   for i = 1, 7 do
-      table.insert(result, rechnen.inbox(" ~"))
-      table.insert(result, " & ")
-   end
-   table.insert(result, [[\\]])
-   for i =1, 7- string.len(tostring(rechnen.kontrolle)) do
-      table.insert(result, " & ")
-   end
-
-   table.insert(result, rechnen.printnumber(rechnen.kontrolle, rechnen.small))
-
-   return table.concat(result)
+   local faktor1 = math.random(899)+100
+   local faktor2 = math.random(899)+100
+   if rechnen.negative_zahl() then faktor1 = faktor1 * -1 end
+   if rechnen.negative_zahl() then faktor2 = faktor2 * -1 end
+   aufgabe.faktor1 = faktor1
+   aufgabe.faktor2 = faktor2
+   aufgabe.operation = operations["mult"]
+   aufgabe.anzahl_hilfszeilen = 3
+   aufgabe.anzahl_hilfskaestchen = 4
+   offset_hilfskaestchen = 0
+   if faktor1 < 0 then offset_hilfskaestchen = offset_hilfskaestchen+1 end
+   if faktor2 < 0 then offset_hilfskaestchen = offset_hilfskaestchen+1 end
+   aufgabe.offset_hilfskaestchen = offset_hilfskaestchen + aufgabe.anzahl_hilfskaestchen
+   aufgabe.kontrolle = aufgabe.operation(faktor1, faktor2)
+   aufgabe.anzahl_loesungskaestchen = tostring(aufgabe.kontrolle)
+   return aufgabe
 end
 
 return rechnen
--- template.lua
+++ template.lua
@@ -1,8 +1,10 @@
 template={}
 
-function template.print_header(tex)
---   tex.print([[\begin{center}\textsf{Multiplikation bis 1.000.000}\end{center}\vspace{1cm}]])
-end
+local operators = {}
+operators["add"] = "$+$"
+operators["subt"] = "$-$"
+operators["mult"] = "\\cdot"
+operators["div"] = "$:$"
 
 function template.print_table_env_begin(tex)
    tex.print([[\begin{tabular}{@{}c@{\hspace{1cm}}c@{\hspace{1cm}}c@{}}]])
@@ -13,7 +15,7 @@ function template.print_table_env_end(tex)
 end   
 
 function template.print_aufgabe_begin(tex)
-   tex.print([[\begin{tabular}{@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}}]])
+   tex.print([[\begin{tabular}{@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}c@{\hspace{1pt}}}]])
 end   
 
 function template.print_aufgabe_end(tex)
@@ -23,8 +25,8 @@ end
 function template.print_aufgaben(tex)
    require "rechnen"
    for i=1, 5 do
-      tex.print(template.print_single_aufgabe(tex))
-      tex.print(" & ")
+--      tex.print(template.print_single_aufgabe(tex))
+--      tex.print(" & ")
       tex.print(template.print_single_aufgabe(tex))
       tex.print(" & ")
       tex.print(template.print_single_aufgabe(tex))
@@ -32,16 +34,87 @@ function template.print_aufgaben(tex)
    end
 end
 
+
+
 function template.print_single_aufgabe(tex)
    require "rechnen"
+   aufgabe = rechnen.create_aufgabe()
    template.print_aufgabe_begin(tex)
-   tex.print(rechnen.create(899,899))
+   local result= {}
+   table.insert(result, template.printnumber(aufgabe.faktor1, rechnen.inbox))
+   table.insert(result, rechnen.inbox(operator))
+   table.insert(result, " & ")
+   table.insert(result, template.printnumber(aufgabe.faktor2, rechnen.inbox))
    tex.print("\\\\")
-   tex.print(rechnen.hilfsbereich())
+   tex.print(template.hilfsbereich(aufgabe))
    tex.print("[1ex]")
-   tex.print(rechnen.loesungszeile())
+   tex.print(template.loesungszeile())
    tex.print("\\\\[1cm]")
    template.print_aufgabe_end(tex)   
+end
+
+function template.printnumber(z, printer)
+   local result = {}
+   local p = printer or function(x) return x end
+   for c in string.gmatch(tostring(z), ".") do
+      if c == '-' then c="$-$" end
+      table.insert(result, p(c))
+      table.insert(result, " & ")
+   end
+   return table.concat(result)
+end
+
+function template.inbox(c)
+   local result = {}
+   table.insert(result, [[\fbox{\parbox[b][2ex][c]{1em}{\centering]])
+   table.insert(result, c)
+   table.insert(result, "}}")
+   return table.concat(result)
+end
+
+function template.small(c)
+   local result = {}
+   table.insert(result, [[{\footnotesize \parbox[t][0ex][t]{1em}{\raggedleft]])
+   table.insert(result, c)
+   table.insert(result, "}}")
+   return table.concat(result)
+end
+
+function template.hilfszeile(z, aufgabe)
+   local result = {}
+   startpos = aufgabe.offset_hilfskaestchen
+   pos = startpos - z
+   for i = 1, pos do
+      table.insert(result, " & ")
+   end
+   for i = 1, aufgabe.anzahl_hilfskaestchen do
+      table.insert(result, rechnen.inbox(" ~"))
+      table.insert(result, " & ")
+   end
+   return table.concat(result)
+end
+
+function template.hilfsbereich(aufgabe)
+   local result= {}
+   for i = 1, aufgabe.anzahl_hilfszeilen do
+      table.insert(result, template.hilfszeile(i, aufgabe))
+      table.insert(result, [[\\]])
+   end
+   return table.concat(result)
+end
+      
+function template.loesungszeile()
+   local result = {}
+   for i = 1, aufgabe.anzahl_loesungskaestchen do
+      table.insert(result, rechnen.inbox(" ~"))
+      table.insert(result, " & ")
+   end
+   table.insert(result, [[\\]])
+   for i =1, aufgabe.anzahl_loesungskaestchen - string.len(tostring(rechnen.kontrolle)) do
+      table.insert(result, " & ")
+   end
+   table.insert(result, template.printnumber(aufgabe.kontrolle, rechnen.small))
+   return table.concat(result)
 end
 
 return template
